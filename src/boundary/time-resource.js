fe94da300a6a0d674495a6f64b852c2769ff6281
rickzx98 <rickzx98@gmail.com>
2016-07-30 20:54:24 +0800

    updates

--- src/boundary/time-resource.js
+++ src/boundary/time-resource.js
@@ -2,14 +2,16 @@
 var Time = require('./time');
 var API = process.env.API_NAME || '/gds/time/';
 
-app.get(API + 'get-today-records/:currenTimeMilis', function (req, res) {
-    var requestHost = req.headers.host;
-    var currenTimeMilis = req.params.currenTimeMilis;
-    Time.getTodayRecords(requestHost, currenTimeMilis, function (err, result) {
-        if (!err) {
-            res.status(200).send(result);
-        } else {
-            res.status(200).send([]);
-        }
+module.exports = function (app) {
+    app.get(API + 'get-today-records/:currenTimeMilis', function (req, res) {
+        var requestHost = req.headers.host;
+        var currenTimeMilis = req.params.currenTimeMilis;
+        Time.getTodayRecords(requestHost, currenTimeMilis, function (err, result) {
+            if (!err) {
+                res.status(200).send(result);
+            } else {
+                res.status(200).send([]);
+            }
+        });
     });
-});
+};
--- src/boundary/time.js
+++ src/boundary/time.js
@@ -4,12 +4,12 @@ var GetTodayRecords = require('../control/time/get-today-records');
 
 module.exports = {
     getTodayRecords: getTodayRecords
-}
+};
 
 function getTodayRecords(host, todayLongValue, callback) {
     new ValidateHost(host, function (errHost) {
         if (errHost) {
-            callback(errHost)
+            callback(errHost);
         } else {
             new GetTodayRecords(todayLongValue, callback);
         }
