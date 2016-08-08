fbb3a18806b9f84d1b80589237829860e006460c
rickzx98 <rickzx98@gmail.com>
2016-08-09 07:31:44 +0800

    added comments

--- src/config/init-services.js
+++ src/config/init-services.js
@@ -4,20 +4,21 @@ var AddServiceAction = require('../control/service/add-service-action');
 var lodash = require('lodash');
 var changeCase = require('change-case');
 var SetDefaultProtocol = require('../control/common/set-default-protocol');
+
 function execute(callback) {
     var servicePorts = [];
-    lodash.forEach(process.env, function (value, key) {
+    lodash.forEach(process.env, function(value, key) {
         if (key.indexOf('_SERVICE_PORT') > 1) {
             var port = {};
             lodash.set(port, 'key', key);
-            new SetDefaultProtocol(value, function (err, httpLink) {
+            new SetDefaultProtocol(value, function(err, httpLink) {
                 lodash.set(port, 'value', httpLink);
             });
             servicePorts.push(port);
         }
     });
     var restServices = {};
-    processPorts(servicePorts, restServices, function (err, result) {
+    processPorts(servicePorts, restServices, function(err, result) {
         if (err) {
             callback(err);
         } else {
@@ -33,12 +34,15 @@ function processPorts(servicePorts, restServices, callback, index) {
     if (index < servicePorts.length) {
         var port = servicePorts[index];
         var serviceName = changeCase.camelCase(port.key.toLowerCase());
-        new CheckAndGetApi(port.value, function (err, api) {
+        console.log('connecting to ' + serviceName + ': ' + port.value);
+        new CheckAndGetApi(port.value, function(err, api) {
             if (err) {
+                console.log('failed connecting to ' + serviceName + ': ' + port.value);
                 callback(err);
             } else {
                 lodash.set(restServices, serviceName, api);
-                new AddServiceAction(api.links, function (errLink, links) {
+                console.log('connected to ' + serviceName + ': ' + port.value);
+                new AddServiceAction(api.links, function(errLink, links) {
                     if (errLink) {
                         callback(errLink);
                     } else {
