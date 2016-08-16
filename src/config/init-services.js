668bc92bc223ab138e89b2dcfdafec3b02e5b53c
rickzx98 <rickzx98@gmail.com>
2016-08-16 10:39:16 +0800

    updates

--- src/config/init-services.js
+++ src/config/init-services.js
@@ -8,7 +8,7 @@ var SetDefaultProtocol = require('../control/common/set-default-protocol');
 function execute(callback) {
     var servicePorts = [];
     lodash.forEach(process.env, function(value, key) {
-        if (key.match(/SERVICE_PORT$/g) && key !== 'USER_SERVICE_ENV_SCHOOL_CONFIG_SERVICE_PORT') {
+        if (key.match(/SERVICE_PORT$/g) && !key.match(/.*_ENV_.*_SERVICE_PORT$/g)) {
             console.log('matches', key);
             var port = {};
             lodash.set(port, 'key', key);
