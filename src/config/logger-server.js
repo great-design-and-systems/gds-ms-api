ebe20c115d59b9c298c0a08af39b7b03960a6afd
rickzx98 <rickzx98@gmail.com>
2016-08-20 01:45:36 +0800

    updates

--- src/config/logger-server.js
+++ src/config/logger-server.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var log4js = require('log4js');
-var LOG_FILE = process.env.APP_LOG || '/app/log/system.log';
+var LOG_FILE = process.env.APP_LOG || 'log/system.log';
 var APP_NAME = process.env.APP_NAME || 'gds_api';
 
-module.exports = function (app) {
+module.exports = function(app) {
     log4js.configure({
         appenders: [
-            { type: 'console' },
+            { type: 'console', filename: LOG_FILE, category: APP_NAME },
             { type: 'file', filename: LOG_FILE, category: APP_NAME }
         ],
         replaceConsole: true
