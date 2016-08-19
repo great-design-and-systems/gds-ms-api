bdeb23192578d079352b1c18617759a213778898
rickzx98 <rickzx98@gmail.com>
2016-08-20 01:00:03 +0800

    updates

--- src/events/importer.events.js
+++ src/events/importer.events.js
@@ -1,19 +1,22 @@
 'use strict';
-var socket = require('socket.io-client')(process.env.IMPORT_SERVICE_PORT);
+var SetDefaultProtocol = require('../control/common/set-default-protocol')
 
 function execute(hostSocket) {
-    console.log('Started listening to ', process.env.IMPORT_SERVICE_PORT);
-    socket.on('import-tracker', function(data) {
-        console.log('have imported ', data);
-        hostSocket.emit('importer-progress', data);
-    });
-    socket.on('import-fail', function(err) {
-        console.error('import has failed', err);
-        hostSocket.emit('importer-fail');
-    });
-    socket.on('import-complete', function() {
-        console.log('import has finished');
-        hostSocket.emit('importer-complete');
+    new SetDefaultProtocol(process.env.IMPORT_SERVICE_PORT, function(err, url) {
+        var socket = require('socket.io-client')(url);
+        console.log('Started listening to ', url);
+        socket.on('import-tracker', function(data) {
+            console.log('have imported ', data);
+            hostSocket.emit('importer-progress', data);
+        });
+        socket.on('import-fail', function(err) {
+            console.error('import has failed', err);
+            hostSocket.emit('importer-fail');
+        });
+        socket.on('import-complete', function() {
+            console.log('import has finished');
+            hostSocket.emit('importer-complete');
+        });
     });
 }
 module.exports = execute;
