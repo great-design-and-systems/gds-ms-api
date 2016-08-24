810d3f19ee15490d183822cba5ff1dcd495f5499
rickzx98 <rickzx98@gmail.com>
2016-08-24 21:30:49 +0800

    updates

--- src/control/common/wake-me-up.js
+++ src/control/common/wake-me-up.js
@@ -1,7 +1,7 @@
 'use strict';
 var http = require('http');
 var HEROKU_DEPLOYED = process.env.HEROKU_DEPLOYED || false;
-var HEROKU_URL_TO_WAKE = process.env.HEROKU_URL_TO_WAKE || 'https://gds-ms-api.herokuapp.com/gds/update-services';
+var HEROKU_URL_TO_WAKE = process.env.HEROKU_URL_TO_WAKE || 'http://gds-ms-api.herokuapp.com/gds/update-services';
 var HEROKU_WAKE_INTERVAL = process.HEROKU_WAKE_INTERVAL || 300000;
 
 module.exports = function () {
