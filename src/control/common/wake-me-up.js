377d9acf0cad77bb12023cfb36a3e29283eab008
rickzx98 <rickzx98@gmail.com>
2016-08-25 10:50:45 +0800

    updates

--- src/control/common/wake-me-up.js
+++ src/control/common/wake-me-up.js
@@ -1,14 +1,25 @@
 'use strict';
 var http = require('http');
 var HEROKU_DEPLOYED = process.env.HEROKU_DEPLOYED || false;
-var HEROKU_URL_TO_WAKE = process.env.HEROKU_URL_TO_WAKE || 'http://gds-ms-api.herokuapp.com/gds/update-services';
+var HEROKU_URL_TO_WAKE = process.env.HEROKU_URL_TO_WAKE || 'http://gds-ms-api.herokuapp.com/gds/update-services;http://gds-terminal.herokuapp.com;http://gds-lrts.herokuapp.com';
 var HEROKU_WAKE_INTERVAL = process.HEROKU_WAKE_INTERVAL || 300000;
 
 module.exports = function () {
     if (HEROKU_DEPLOYED) {
         console.log('WAKE ME UP BEFORE YOU GO GO!');
-        setInterval(function () {
-            http.get(HEROKU_URL_TO_WAKE);
-        }, HEROKU_WAKE_INTERVAL); // every 5 minutes (300000)
+        if (HEROKU_URL_TO_WAKE.indexOf(';') > -1) {
+            setInterval(function () {
+                var urls = HEROKU_URL_TO_WAKE.split(';');
+                for (var i = 0; i < urls.length; i++) {
+                    var url = urls[i];
+                    http.get(url);
+                }
+            }, HEROKU_WAKE_INTERVAL); // every 5 minutes (300000)
+        } else {
+            setInterval(function () {
+                http.get(HEROKU_URL_TO_WAKE);
+            }, HEROKU_WAKE_INTERVAL); // every 5 minutes (300000)
+        }
+
     }
 };
